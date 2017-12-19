f26a3d3beb3dd5563ababd1dda8e082fa7177bad
amannocci <adrien.mannocci@gmail.com>
2017-12-19 10:46:38 +0100

    [Added] Coverage for dead letter monitor
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

---
+++ core/src/test/scala/io/techcode/streamy/util/monitor/DeadLetterMonitorSpec.scala
@@ -0,0 +1,63 @@
+/*
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.techcode.streamy.util.monitor
+
+import akka.actor.{ActorSystem, DeadLetter, Kill, Props}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Matchers, WordSpecLike}
+
+/**
+  * Dead letter monitoring spec.
+  */
+class DeadLetterMonitorSpec extends TestKit(ActorSystem("PluginSpec"))
+  with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll with BeforeAndAfter {
+
+  implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system))
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "Dead letter monitoring" can {
+    "be started and stopped" in {
+      val deadLetterMonitor = system.actorOf(Props[DeadLetterMonitor])
+      deadLetterMonitor ! Kill
+    }
+
+    "handle correctly dead letter" in {
+      val deadLetterMonitor = system.actorOf(Props[DeadLetterMonitor])
+      system.eventStream.subscribe(deadLetterMonitor, classOf[DeadLetter])
+      system.eventStream.publish(DeadLetter)
+      deadLetterMonitor ! Kill
+    }
+
+    "not receive message by default" in {
+      val deadLetterMonitor = system.actorOf(Props[DeadLetterMonitor])
+      deadLetterMonitor ! "test"
+      deadLetterMonitor ! Kill
+    }
+  }
+
+}
