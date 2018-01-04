daa41746b32eedb7e129a4d4f1a39f77ae561cfd
amannocci <adrien.mannocci@gmail.com>
2018-01-04 16:32:41 +0100

    [Added] Support env templating for configurations
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

---
+++ core/src/universal/bin/make-template.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob
+
+is_install() {
+   # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+# Make template if supported
+if [ $(is_install envtpl) == 1 ]; then
+  echo "{\"level\":\"info\",\"type\":\"entrypoint\",\"message\":\"envtpl is installed\"}"
+  for file in $PWD/conf/*.tpl; do
+    echo "{\"level\":\"info\",\"type\":\"entrypoint\",\"message\":\"Make template $file\"}"
+    filename="$(basename "$file")"
+    filename="${filename%.*}"
+    envtpl < $file > "$PWD/conf/$filename"
+  done
+else
+  echo "{\"level\":\"info\",\"type\":\"entrypoint\",\"message\":\"envtpl isn't installed\"}"
+fi
---
+++ core/src/universal/bin/setup-container.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+ceiling() {
+  awk -vnumber="$1" -vdiv="$2" '
+    function ceiling(x){
+      return x%1 ? int(x)+1 : x
+    }
+    BEGIN{
+      print ceiling(number/div)
+    }
+  '
+}
+
+# Based on the cgroup limits, figure out the max number of core we should utilize
+core_limit() {
+  # Manually defined
+  if [ "x$JVM_CPU_LIMIT" != x ]; then
+    echo "${JVM_CPU_LIMIT}"
+    return
+  fi
+
+  # Number of cpu on host
+  local cpu_limit="$(nproc)"
+
+  # Read cgroups limit
+  local cpu_period_file="/sys/fs/cgroup/cpu/cpu.cfs_period_us"
+  local cpu_quota_file="/sys/fs/cgroup/cpu/cpu.cfs_quota_us"
+  if [ -r "${cpu_period_file}" ]; then
+    local cpu_period="$(cat ${cpu_period_file})"
+
+    if [ -r "${cpu_quota_file}" ]; then
+      local cpu_quota="$(cat ${cpu_quota_file})"
+      # cfs_quota_us == -1 --> no restrictions
+      if [ "x$cpu_quota" != "x-1" ]; then
+        ceiling "$cpu_quota" "$cpu_period"
+      else
+        echo "$cpu_limit"
+      fi
+    else
+      echo "$cpu_limit"
+    fi
+  else
+    echo "$cpu_limit"
+  fi
+}
+
+memory_limit() {
+  # Manually defined
+  if [ "x$JVM_MEMORY_LIMIT" != x ]; then
+    echo "${JVM_MEMORY_LIMIT}"
+    return
+  fi
+
+  # Max memory of host
+  local mem_limit="$(free -b | grep "Mem:" | awk '{print $2;}')"
+
+  # High number which is the max limit unti which memory is supposed to be
+  # unbounded.
+  local max_mem_unbounded="$(cat /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes)"
+  local mem_file="/sys/fs/cgroup/memory/memory.limit_in_bytes"
+  if [ -r "${mem_file}" ]; then
+    local max_mem="$(cat ${mem_file})"
+    if [ ${max_mem} -lt ${max_mem_unbounded} ]; then
+      echo "${max_mem}"
+    else
+      echo "${mem_limit}"
+    fi
+  else
+    echo "${mem_limit}"
+  fi
+}
+
+# Cpu Limit
+export JVM_CPU_LIMIT="$(core_limit)"
+export JVM_CPU_LIMIT_X2="$((2 * $JVM_CPU_LIMIT))"
+echo "Jvm Cpu Limit: ${JVM_CPU_LIMIT}"
+echo "Jvm Cpu Limit * 2: ${JVM_CPU_LIMIT_X2}"
+addJava "-XX:ParallelGCThreads=${JVM_CPU_LIMIT}"
+addJava "-XX:ConcGCThreads=${JVM_CPU_LIMIT}"
+addJava "-Djava.util.concurrent.ForkJoinPool.common.parallelism=${JVM_CPU_LIMIT}"
+
+# Memory Limit
+export JVM_MEMORY_LIMIT="$(memory_limit)"
+echo "Jvm Memory Limit: ${JVM_MEMORY_LIMIT}"
+max_mem="${JVM_MEMORY_LIMIT}"
+ratio=${JVM_MEMORY_RATIO:-50}
+mx=$(echo "${max_mem} ${ratio} 1048576" | awk '{printf "%d\n" , ($1*$2)/(100*$3) + 0.5}')
+addJava "-Xmx${mx}M"
+unset max_mem
+unset ratio
+unset mx
