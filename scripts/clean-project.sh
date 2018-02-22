a2739f0ea7adb57ed5ec1a868d0ec8a195426bea
amannocci <adrien.mannocci@gmail.com>
2018-02-22 12:28:29 +0100

    [Added] Provide a fast way to clean project
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

---
+++ scripts/clean-project.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Load common
+source ${PWD}/scripts/common.sh
+
+# Remove every target in sub projects
+function clean {
+  rm -rf $1 2> /dev/null
+}
+clean ${BASE_PROJECT}/core/target
+clean ${BASE_PROJECT}/plugin-fingerprint/target
+clean ${BASE_PROJECT}/plugin-json/target
+clean ${BASE_PROJECT}/plugin-metric/target
+clean ${BASE_PROJECT}/plugin-syslog/target
+clean ${BASE_PROJECT}/plugin-graphite/target
+clean ${BASE_PROJECT}/project/target
+clean ${BASE_PROJECT}/project/project
+
+exit 0
