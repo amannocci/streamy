d1cd81615e6d25b752eb752d4bc259883ae37f39
amannocci <adrien.mannocci@gmail.com>
2019-10-23 18:21:12 +0200

    [Fixed] Correct shell linter
    Signed-off-by: amannocci <adrien.mannocci@gmail.com>

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Load common
+# # shellcheck disable=SC1090
 source "${PWD}/scripts/common.sh"
 
 # Extract current version
@@ -8,6 +9,6 @@ current_version=$(grep -oP 'version := "(.*)"' "${BASE_PROJECT}/build.sbt" | cut
 
 # Ask for new version
 echo "Current version: ${current_version}"
-read -p 'New version: ' new_version
+read -pr 'New version: ' new_version
 grep -R "version = \"${current_version}\"" "${BASE_PROJECT}" | cut -f1 -d':' | xargs -i sed -i "s/version = \"${current_version}\"/version = \"${new_version}\"/g" {}
 sed -i "s/version := \"${current_version}\"/version := \"${new_version}\"/g" "${BASE_PROJECT}/build.sbt"
